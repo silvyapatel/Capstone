0ce02ce14940e4ffdd6a609e323d32b5ebf00e34
silvyapatel <silvya.patel@outlook.com>
2024-11-20 06:27:27 +0000

    Delete capstonebackend directory

--- capstonebackend/src/main/java/com/example/capstone/capstonebackend/Model/AccountType.java
+++
@@ -1,7 +0,0 @@
-package com.example.capstone.capstonebackend.Model;
-
-public enum AccountType {
-    SAVINGS,  // Savings account
-    CHECKING, // Checking account
-    BUSINESS  // Business account
-}
